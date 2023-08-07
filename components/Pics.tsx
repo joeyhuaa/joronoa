5a950b17cd2658ad8f4ade80a9d12e80a9983d88
Joey Hua <joeyhua17@gmail.com>
2023-08-06 21:19:11 -0700

    initial setup and Pics

---
+++ components/Pics.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+import supernova_art from '../public/assets/supernova.jpg'
+import soulfection_art from '../public/assets/soulfection.jpg'
+import prof_pic from '../public/assets/prof-pic-square.jpg'
+import headlightsnight from '../public/assets/headlightsnight.jpg'
+import tylerbgb from '../public/assets/tylerbluegreenblack.jpg'
+import tylerbp from '../public/assets/tylerbluepinkoutline.jpg'
+import tylerog from '../public/assets/tylerorangegreenoutline.jpg'
+import tylerrb from '../public/assets/tylerredblkpoints.jpg'
+import { StaticImageData } from 'next/image'
+
+const pics: StaticImageData[] = [prof_pic, tylerbgb, tylerog, tylerbp, tylerrb]
+
+export default function Pics() {
+  let [picIndex, setPicIndex] = useState(0)
+
+  let changePic = () => {
+    let newIndex = picIndex + 1
+    if (newIndex < pics.length) setPicIndex(newIndex)
+    else setPicIndex(0)
+  }
+
+  return (
+    <img id='random-pic' src={pics[picIndex].src} alt='pic' onClick={changePic} width={250} height={250} />
+  )
+}
