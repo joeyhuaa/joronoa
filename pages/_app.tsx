c9dd5bee2810647a8f5b9778bc9fcabecebc12e9
Joey Hua <joeyhua17@gmail.com>
2023-12-31 05:12:35 -0600

    fix build error, refine app loading fade

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,8 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import styled, { keyframes } from 'styled-components'
-import { Suspense, useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { useState, useEffect } from 'react'
 import AppLoading from './loading'
 
 import LinkedSidebar from '@/components/LinkedSidebar'
@@ -46,63 +46,40 @@ export default function App({ Component, pageProps }: AppProps) {
   }
 
   return (
-    <AppContainer loading={loading}>
-      <Suspense fallback={<AppLoading />}>
-        {loading ? <AppLoading /> : (
-          <>
-            <Head>
-              <link href="https://fonts.googleapis.com/css?family=Tilt Prism" rel="stylesheet" />
-              <link href="https://fonts.googleapis.com/css2?family=Tilt+Warp&display=swap" rel="stylesheet" />
-              <link href="https://fonts.googleapis.com/css2?family=Ruwudu&display=swap" rel="stylesheet" />
-              <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"></link>
-            </Head>
-            <div style={{display: 'flex'}}>
-              <div id='main-menu'>
-                <Link href='/' onClick={() => handleClick('')}>
-                  <h1 className='text-6xl/loose pl-5' onMouseEnter={() => setNameHovered(true)} onMouseLeave={() => setNameHovered(false)}>
-                    {nameHovered ? <HoverText>joey 花</HoverText> : <UnHoverText>joronoa</UnHoverText>}
-                  </h1>
-                </Link>
-                <LinkedSidebar 
-                  pages={pages}
-                  links={links}
-                  selected={section}
-                />
-              </div>
-              <div style={{flex: '75%'}}>
-                <Component {...pageProps} />
-              </div>
+    <>      
+      {loading ? <AppLoading /> : (
+        <AppContainer>
+          <Head>
+            <link href="https://fonts.googleapis.com/css?family=Tilt Prism" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=Tilt+Warp&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=Ruwudu&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"></link>
+          </Head>
+          <div style={{display: 'flex'}}>
+            <div id='main-menu'>
+              <Link href='/' onClick={() => handleClick('')}>
+                <h1 className='text-6xl/loose pl-5' onMouseEnter={() => setNameHovered(true)} onMouseLeave={() => setNameHovered(false)}>
+                  {nameHovered ? <HoverText>joey 花</HoverText> : <UnHoverText>joronoa</UnHoverText>}
+                </h1>
+              </Link>
+              <LinkedSidebar 
+                pages={pages}
+                links={links}
+                selected={section}
+              />
             </div>
-          </>
-        )}
-      </Suspense>
-    </AppContainer>
+            <div style={{flex: '75%'}}>
+              <Component {...pageProps} />
+            </div>
+          </div>
+        </AppContainer>
+      )}
+    </>
   )
 }
 
-const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-`;
-
-const fadeOut = keyframes`
-  from {
-    opacity: 1;
-  }
-  to {
-    opacity: 0;
-  }
-`;
-
 const AppContainer = styled.div`
   padding: 30px;
-  transition: opacity 0.5s ease-in-out;
-  opacity: ${(props) => (props.loading ? 0 : 1)};
-  animation: ${(props) => (props.loading ? fadeOut : fadeIn)} 0.5s ease-in-out;
 `
 const HoverText = styled.span`
   animation: fadeIn 0.6s ease-in-out;
